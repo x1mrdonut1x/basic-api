f1e1508b0bdb55db7f8303831531be9da9d23c4b
Alex Niznik <alex@niznik.info>
2020-01-21 08:57:10 +0300

    feat: register and login

--- app/routes/auth.js
+++ app/routes/auth.js
@@ -2,34 +2,69 @@ require('dotenv').config()
 const express = require('express');
 const app = module.exports = express();
 const fs = require('fs');
-const jwt = require('jsonwebtoken')
+const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-app.post('/auth/register', (req, res) => {
-  fs.readFile(__dirname + "/" + "db.json", 'utf8', (err, data) => {
-    let users = JSON.parse(data);
+app.post('/register', (req, res) => {
+
+  fs.readFile(__dirname + "/../" + "db.json", 'utf8', (err, data) => {
+    data = JSON.parse(data);
+    let users = data.users;
+    const {
+      email,
+      password
+    } = req.body;
+    console.log(email, password)
+
+    if (users.findIndex(u => u.email === email) > -1) return res.json(400).end()
+
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(password, salt);
+
+    console.log(salt, hash)
     const newUser = {
-      email: req.body.username,
-      id: users.length
+      id: `user-${users.length + 1}`,
+      email,
+      password: hash,
+      //   salt,
+      basket: [],
     }
-    users.push(newUser)
 
-    fs.writeFile(__dirname + "/" + "db.json", JSON.stringify(users, null, 2), function(err) {
+    users.push(newUser)
+    data.users = users
+    fs.writeFile(__dirname + "/../" + "db.json", JSON.stringify(data, null, 2), function(err) {
       if (err) {
         return console.log(err);
       }
       res.json(newUser);
     });
+
+
   })
 })
 
-app.post('/auth/login', (req, res) => {
+app.post('/login', (req, res) => {
+  const {
+    email,
+    password
+  } = req.body;
 
-  const id = req.body.id;
-  const user = {
-    id
-  }
-  const accessToken = jwt.sign(user, process.env.TOKEN_SECRET)
-  res.json({
-    accessToken
+  fs.readFile(__dirname + "/../" + "db.json", 'utf8', (err, data) => {
+    let users = JSON.parse(data).users;
+
+    const user = users.find(u => u.email === email)
+    if (user == null) return res.sendStatus(400).end()
+    
+    const result = bcrypt.compareSync(password, user.password)
+
+
+    if (!result) return res.sendStatus(401)
+
+    const accessToken = jwt.sign({
+      id: user.id
+    }, process.env.TOKEN_SECRET)
+    res.json({
+      accessToken
+    })
   })
 })
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const app = module.exports = express();
 
-app.get('/', (req, res) => res.end('Working!'))
+// app.get('/', (req, res) => res.end('Working!'))
 
+app.use('/', require('./auth'))
 app.use('/user', require('./user'))
-app.use('/auth', require('./auth'))
 app.use('/products', require('./products'))
 
 
