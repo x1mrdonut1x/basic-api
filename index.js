215965aa2d36f9f13a9025debfcc2d769e5b568a
Alex Niznik <alex@niznik.info>
2020-01-22 12:30:44 +0300

    fix: fix server cors

--- index.js
+++ index.js
@@ -1,29 +1,21 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
+
 const {
   readFile,
   writeFile,
-} = require('./utils')
+} = require('./server/utils')
 
-const dataPath = `${__dirname}/databases/db.json`
+const dataPath = `${__dirname}/server/databases/db.json`
 
-const dependencies = {
+const service = {
   read: readFile(dataPath),
   write: writeFile(dataPath)
 }
-
+const routes = require('./server')(service)
 
 app.use(cors())
-
-app.use(express.json())
-
-app.use('/auth', require('./routes/auth')(dependencies))
-app.use('/user', require('./routes/user')(dependencies))
-app.use('/user/basket', require('./routes/basket')(dependencies))
-app.use('/products', require('./routes/products')(dependencies))
-
-app.all('*', (req, res) => res.sendStatus(404))
-
+app.use(routes)
 
 app.listen(4000)
